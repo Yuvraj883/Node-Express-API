da668d12afe07e0fefefa6c44346a5b6f2f151ae
Yuvraj883 <kingyuvi88@gmail.com>
2022-07-20 01:32:02 +0530

    Added Routes

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from 'express'; 
 import bodyParser from 'body-parser'; 
+import send from 'send';
+import usersRoute from './Routes/users.js'
 
 
 const app = express(); 
 const PORT = 8000; 
 
 app.use(bodyParser.json()); 
+app.get('/', (req, res)=>{
+    res.send("Hello from Home Page");
+})
+
+app.use('/users', usersRoute)
 
 app.listen(PORT, ()=>console.log(`Server running on port: http://localhost:${PORT}`)); 
