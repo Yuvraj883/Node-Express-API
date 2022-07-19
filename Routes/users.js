e0928920951fb65af44e4cd995c5bd35addf85fa
Yuvraj883 <kingyuvi88@gmail.com>
2022-07-20 03:13:51 +0530

    Added get  users/:id

--- Routes/users.js
+++ Routes/users.js
@@ -2,11 +2,12 @@ import express from 'express';
 import {v4 as uuidv4} from 'uuid';
 const router = express.Router(); 
 let users=[
-    // {
-    //     "firstName":"God",
-    //     "lastName":"Yuvi",
-    //     "age":99999999999
-    // },
+    {
+        "firstName":"God",
+        "lastName":"Yuvi",
+        "age":99999999999,
+        "id":"1"
+    }
     // { 
     //  "firstName":"Aami",
     // "lastName":"KKR",
@@ -31,5 +32,16 @@ router.post('/', (req, res)=>{
     //console.log("Posted successfully!", req.body); 
     // res.send("Posted Successfully!");
 })
+
+
+router.get('/:id',(req, res)=>{
+    // res.send("Get request with UserId")
+    const { id }= req.params;
+    const foundUser = users.find((user) => user.id ===id); 
+    if(foundUser) res.send(foundUser); 
+    else res.send("User not found"); 
+    console.log(foundUser);
+})
+
 
 export default router; 
